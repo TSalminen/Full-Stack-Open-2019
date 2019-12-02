a97377b6223a2a82e0163ec9f118bc0b00630757
Tuukka Salminen <tuukka.salminen@helsinki.fi>
2019-12-02 21:04:00 +0200

    1.7 unicafe step2 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -6,16 +6,41 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const setGoodToValue = (value) => setGood(value)
-  const setNeutralToValue = (value) => setNeutral(value)
-  const setBadToValue = (value) => setBad(value)
+  const handleGoodClicks = () => {
+    setAll(all + 1)
+    setGood(good + 1)
+  }
+  const handleNeutralClicks = () => {
+    setAll(all + 1)
+    setNeutral(neutral + 1)
+  }
+  const handleBadClicks = () => {
+    setAll(all + 1)
+    setBad(bad + 1)
+  }
+  
+  const calculateAverage = () => {
+      if (all === 0) {
+          return 0
+      }
+      return (good - bad) / all
+  }
+
+  const calculatePositive = () => {
+      if (all === 0) {
+        return 0
+      }
+      return good / all * 100
+  }
 
   const Button = ({ onClick, text}) => (
       <button onClick={onClick}>
           {text}
       </button>
   )
+
 
   const Display = ({ category, value }) => <div>{category} {value}</div>
 
@@ -24,15 +49,18 @@ const App = () => {
         <h1>give feedback</h1>
         <div>
             <Button 
-                onClick={() => setGoodToValue(good + 1)}
+                // onClick={() => setGoodToValue(good + 1)}
+                onClick={() => handleGoodClicks()}
                 text='good'
             />
             <Button 
-                onClick={() => setNeutralToValue(neutral + 1)}
+                // onClick={() => setNeutralToValue(neutral + 1)}
+                onClick={() => handleNeutralClicks()}
                 text='neutral'
             />
             <Button 
-                onClick={() => setBadToValue(bad + 1)}
+                // onClick={() => setBadToValue(bad + 1)}
+                onClick={() => handleBadClicks()}
                 text='bad'
             />
         </div>
@@ -40,6 +68,10 @@ const App = () => {
         <Display category='good' value={good} />
         <Display category='neutral' value={neutral} />
         <Display category='bad' value={bad} />
+        <Display category='all' value={all} />
+        <Display category='average' value={calculateAverage()} />
+        <Display category='positive' value={calculatePositive() + ' %'} />
+
     </>
   )
 }
