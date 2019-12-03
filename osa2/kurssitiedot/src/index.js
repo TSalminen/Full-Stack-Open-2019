6cd0deb04af7d404bc65506647113354b6221c09
Tuukka Salminen <tuukka.salminen@helsinki.fi>
2019-12-03 17:34:07 +0200

    2.5 erillinen moduuli tehty

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,47 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from './components/Course'
 
-const Header = ({course}) => <h1>{course['name']}</h1>
-
-const Content = ({parts}) => {
-    // console.log(parts)
-    const rows = () => parts.map(part => 
-        <Part 
-            key={part.id}
-            part={part} 
-        />
-    )
-    return (
-        <div>
-            {rows()}
-        </div>
-
-    )
-}
-
-const Part = ({part}) => <p>{part['name']} {part['exercises']}</p>
-
-const Total = ({parts}) => {
-    const reducer = (accumulator, currentValue) => accumulator + currentValue
-    const exerciseList = parts.map(part => part['exercises'])
-    // console.log(exerciseList)
-    const numberOfExercises = exerciseList.reduce(reducer)
-    // console.log(numberOfExercises)
-    
-    return (
-        <p><b>total of {numberOfExercises} exercises</b></p>
-    )
-}
-
-const Course = ({course}) => {
-    return (
-        <>
-        <Header course={course} />
-        <Content parts={course['parts']} />
-        <Total parts={course['parts']} />
-        </>
-    )
-}
 
 const Courses = ({courses}) => {
     // lisätään kursseille uniikit id-numerot
