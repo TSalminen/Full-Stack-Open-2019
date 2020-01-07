6cf2adbfdd3a86ba2f5d5b34b6cf73d55cb59093
Tuukka Salminen <tuukka.salminen@helsinki.fi>
2020-01-07 17:55:09 +0200

    4.2 commit2 config.js erotettu

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const app = require('./app')
 const http = require('http')
+const config = require('./utils/config')
 const mongoose = require('mongoose')
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+// const PORT = 3003
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
